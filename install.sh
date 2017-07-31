19e87006df928b251f7f629086ed963d5ab736f1
Junyoung Lim <tlstjstksth@gmail.com>
2017-07-31 13:09:09 +0900

    Fix typo in install_vim()

--- install.sh
+++ install.sh
@@ -111,7 +111,8 @@ function install_zsh() {
 function install_vim() {
   if [ "$(uname)" = "Darwin" ]; then
     brew install vim
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \ https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+          https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     cp_backup vimrc
   fi
 }
